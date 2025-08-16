3bd08dc260bb1ba72c37cc3f4cb2dbb432fcf0e3
leNITHISH <the.nithish.siva.kumar@gmail.com>
2025-08-16 20:11:04 +0530

    oiled up

--- oiledUpPhi/oiler.java
+++ oiledUpPhi/oiler.java
@@ -1,1 +1,10 @@
 
+static int[] phiSieve(int n){
+    int[] phi = new int[n+1]
+
+    for(int i=0;i<=n;i++) phi[i]=i
+
+    for(int i=2;i<=n;i++) if(phi[i]==i) for(int j=i;j<=n;j+=i) phi[j]=phi[j]/i*(i-1)
+
+    return phi
+}
